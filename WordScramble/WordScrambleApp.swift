e3e7cc59d832da0ab5ee8f32cf7a9ab13aa724ff
Arshya GHAVAMI <arshyaghavami@Arshyas-MacBook-Air.local>
2021-01-06 10:59:27 -0800

    First commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Arshya GHAVAMI on 1/4/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
